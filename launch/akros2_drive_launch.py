74917711f998975c99867d856857e306f2cb8930
Aditya Kamath <adityakamath@live.com>
2023-10-06 19:25:11 +0200

    Update akros2_drive_launch.py

--- launch/akros2_drive_launch.py
+++ launch/akros2_drive_launch.py
@@ -56,7 +56,7 @@ def generate_launch_description():
         DeclareLaunchArgument(
             name='joy_config',
             default_value='ps4',
-            description='Joystick Configuration: ps4, stadia, ps3'),
+            description='Joystick Configuration: ps4, stadia, ps3, sn30pro[x, y, b]'),
         
         GroupAction(
             condition=IfCondition(LaunchConfiguration('twist_joy')),
