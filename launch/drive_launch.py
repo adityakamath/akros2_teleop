d75c6c83d3fe4da2ce6aa8c30c3b9bae5d866721
adityakamath <adityakamath@live.com>
2023-11-19 14:20:50 +0000

    update twist mixer

--- launch/drive_launch.py
+++ launch/drive_launch.py
@@ -48,9 +48,9 @@ def generate_launch_description():
             name='twist_mixer',
             output='screen',
             remappings=[
-                ('/teleop_vel', '/drive/cmd_vel'), # change from /drive/cmd_vel to  /joy_vel once micro-ros remapping and twist_mixer issues are fixed
+                ('/teleop_vel', '/joy_vel'), # change from /drive/cmd_vel to  /joy_vel once micro-ros remapping and twist_mixer issues are fixed
                 ('/auto_vel', '/nav_vel'),
-                ('/mix_vel', '/cmd_vel'), # change from /cmd_vel to  /drive/cmd_vel once twist_mixer issues are fixed
+                ('/mix_vel', '/drive/cmd_vel'), # change from /cmd_vel to  /drive/cmd_vel once twist_mixer issues are fixed
                 ('/mode', '/drive/mode'), # temporary, to be removed once micro-ros remapping is done
             ]),
     ])
