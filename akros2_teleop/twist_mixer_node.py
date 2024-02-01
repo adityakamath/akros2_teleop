e711763ca246e85f0168704bbd7fbfa08b6fd933
Aditya Kamath <adityakamath@live.com>
2024-02-01 19:37:25 +0100

    Update twist_mixer_node.py

--- akros2_teleop/twist_mixer_node.py
+++ akros2_teleop/twist_mixer_node.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/usr/bin/env python3
-
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
